178ceab8de3585bc6329eab8683dafa4f82f1c4e
Soumyaranjan2021 <88780525+Soumyaranjan2021@users.noreply.github.com>
2022-04-01 12:00:19 +0530

    settings variables modified

--- downloader/settings.py
+++ downloader/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h+0+1=-^py+t$@-p59(0&98t6$fa-h$22p@6u#4%9-6k&wsl(x'
+# SECRET_KEY = 'django-insecure-h+0+1=-^py+t$@-p59(0&98t6$fa-h$22p@6u#4%9-6k&wsl(x'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['social-genie.herokuapp.com', 'localhost']
 
 
 # Application definition
