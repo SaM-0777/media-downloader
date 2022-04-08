fba4e29857d0cd7bfceb4ad02c8f8cdc2041f595
Soumyaranjan2021 <88780525+Soumyaranjan2021@users.noreply.github.com>
2022-04-08 20:29:19 +0530

    No New changes

--- downloader/settings.py
+++ downloader/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h+0+1=-^py+t$@-p59(0&98t6$fa-h$22p@6u#4%9-6k&wsl(x'
+# SECRET_KEY = 'django-insecure-h+0+1=-^py+t$@-p59(0&98t6$fa-h$22p@6u#4%9-6k&wsl(x'
 
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY') if os.environ.get('SECRET_KEY') else 'django-insecure-h+0+1=-^py+t$@-p59(0&98t6$fa-h$22p@6u#4%9-6k&wsl(x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if os.environ.get('DEBUG') == "False" else True
